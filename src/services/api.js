16a18b6969c930fb998364c13b5701d4e936d9df
Patel-Pratham-3492 <patelpratham8320@gmail.com>
2026-01-30 14:10:05 -0600

    final deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
-// src/services/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // change later if needed
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
 });
 
 export default API;
